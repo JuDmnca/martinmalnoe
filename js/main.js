be16d3af9844717306d61bc5691a6dbb4567c45a
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2020-12-16 15:00:27 +0100

    :tada: initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+if (window.matchMedia("(min-width: 600px)").matches) {
+    const scene = document.getElementById('scene')
+    const parallaxInstance = new Parallax(scene, {
+        pointerEvents: true,
+        invertX: false,
+        invertY: false
+    })
+}
+
+let el
+function defineEl(element){
+    el = element
+}
+
+var index = 1;
+var i = 1
+function changeURL(el){
+    if(el != null){
+        while(i == index){
+            i = Math.floor(Math.random() * 8); // Nbr de photo
+        }
+        index = i
+        el.classList.remove('fadeIn')
+        el.classList.add('fadeOut')
+        setTimeout(function () {
+            el.src = 'IMG/bg/bg-0' + index + '.jpg';
+            setTimeout(function () {
+                el.classList.remove('fadeOut')
+                el.classList.add('fadeIn')
+            }, 600);
+
+        }, 1000);
+    }
+}
+
+function ScrollTo(player){
+    target = document.getElementById(player);
+    target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    });
+}
+
+setInterval(function() { changeURL(el); }, 7000); // Il faut changer ce nombre en millisecondes pour choisir le temps entre chaque photos
---
+++ js/slider.js
@@ -0,0 +1,19 @@
+const mySwiper = new Swiper('.swiper-container', {
+  speed: 1,
+  loop: true,
+  allowTouchMove: false,
+  preventClicks: false,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  preloadImages: false,
+  lazy: true,
+  lazy: {
+    loadPrevNext: true,
+  },
+})
+
+mySwiper.on('click', function() {
+  mySwiper.slideNext(1, false)
+})
