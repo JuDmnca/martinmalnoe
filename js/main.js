e2bd577c4ec4373263d2b58d63a0013ada330af0
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2021-01-14 00:38:54 +0100

    :lipstick: Add assets & style for desktop

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ function defineEl(element){
     el = element
 }
 
-var index = 1;
+var index = 1
 var i = 1
 function changeURL(el){
     if(el != null){
         while(i == index){
-            i = Math.floor(Math.random() * 8); // Nbr de photo
+            i = Math.floor(Math.random() * 8) // Nbr de photo
         }
         index = i
         el.classList.remove('fadeIn')
@@ -27,18 +27,17 @@ function changeURL(el){
             setTimeout(function () {
                 el.classList.remove('fadeOut')
                 el.classList.add('fadeIn')
-            }, 600);
-
-        }, 1000);
+            }, 600)
+        }, 1000)
     }
 }
 
 function ScrollTo(player){
-    target = document.getElementById(player);
+    target = document.getElementById(player)
     target.scrollIntoView({
         behavior: 'smooth',
-        block: 'center'
-    });
+        block: 'start'
+    })
 }
 
 setInterval(function() { changeURL(el); }, 7000); // Il faut changer ce nombre en millisecondes pour choisir le temps entre chaque photos
